07f5d77426506ff461477c99e2b4454bffd03862
Mehdi <mabdelou@brocade.com>
2016-10-27 18:21:34 +0200

    first commit

---
+++ actions/del_ip_route.py
@@ -0,0 +1,35 @@
+import requests
+import warnings
+import time
+import json
+
+from st2actions.runners.pythonrunner import Action
+
+class delIpRoute(Action):
+    def run(self, deviceIP, login, password, cmd_path, subnet):
+        
+        headers = {
+            "accept": "application/json",
+            "content-length": "0"
+        }
+
+        subnet = subnet.replace("/", "%2F")
+        url_base = "https://" + deviceIP + "/" + cmd_path
+        url = url_base + "/delete/protocols/static/route/" + subnet
+
+        print("\n")
+        print("Sending REST call: PUT " + url)
+
+        with warnings.catch_warnings():
+            warnings.simplefilter("ignore")
+            cmd_response = requests.put(url, auth=(login, password), headers=headers, verify=False)
+            cmd_response_code = cmd_response.status_code
+            cmd_response_code = str(cmd_response_code)
+            print("Response code: " + cmd_response_code)
+        
+        try:
+            data = json.loads(cmd_response.text)
+            print("Response body: ")
+            print json.dumps(data, sort_keys=True, indent=4)
+        except:
+            print ("Response body is empty")
---
+++ actions/show_system_memory.py
@@ -0,0 +1,42 @@
+import requests
+import warnings
+import time
+import json
+
+from st2actions.runners.pythonrunner import Action
+
+class showSystemMemory(Action):
+    def run(self, deviceIP, login, password):
+        
+        headers = {
+            "accept": "application/json",
+            "content-length": "0"
+        }
+
+        url_base = "https://" + deviceIP + "/rest/op/"
+        url = url_base + "show/system/memory"
+
+        print("\n")
+        print("Sending REST call: POST " + url)
+
+        with warnings.catch_warnings():
+            warnings.simplefilter("ignore")
+            cmd_response = requests.post(url, auth=(login, password), headers=headers, verify=False)
+            cmd_response_code = cmd_response.status_code
+            cmd_response_code = str(cmd_response_code)
+            print("Response code: " + cmd_response_code)
+        
+            if cmd_response.status_code == 201:
+                cmd_path = cmd_response.headers["Location"]
+                url = "https://" + deviceIP + "/" + cmd_path
+                print("Sending REST call: GET " + url)
+                with warnings.catch_warnings():
+                    warnings.simplefilter("ignore")
+                    output_response = requests.get(url, auth=(login, password), headers=headers, verify=False)
+                    cmd_response_code = cmd_response.status_code
+                    cmd_response_code = str(cmd_response_code)
+                    print("Response code: " + cmd_response_code)
+                if output_response.status_code == 200:
+                    print("\n")
+                    print output_response.text
+        
